8c42f5f1f5c3c5379265407af732be9667ed3fcb
Julian Kreuzberger <jkreuzbe@htl-steyr.ac.at>
2025-11-24 13:54:59 +0100

    made changes to ProjektMain

--- .idea/ProjektMain.java
+++ .idea/ProjektMain.java
@@ -2,5 +2,8 @@ class ProjektMain{
     public static void main(String[] args){
         mleimhof mx=new mleimhof();
         mx.tell();
+
+        jkreuzbe jk = new jkreuzbe();
+        jk.tell();
     }
 }
